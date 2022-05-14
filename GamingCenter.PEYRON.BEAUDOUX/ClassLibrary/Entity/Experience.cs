941128cfc25b7455e04ece9268f34e15c727ba72
Hugo PEYRON <91185650+hupeyron@users.noreply.github.com>
2022-05-14 08:55:43 +0200

    add solution to project

---
+++ GamingCenter.PEYRON.BEAUDOUX/ClassLibrary/Entity/Experience.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassLibrary.Entity
+{
+    public class Experience
+    {
+        private int id;
+        private string joueur;
+        private float tempsDeJeu;
+        private float pourcentage;
+
+        public Experience(int id, string joueur, float tempsDeJeu, float pourcentage)
+        {
+            Id = id;
+            Joueur = joueur;
+            TempsDeJeu = tempsDeJeu;
+            Pourcentage = pourcentage;
+        }
+
+        public int Id { get => id; set => id = value; }
+        public string Joueur { get => joueur; set => joueur = value; }
+        public float TempsDeJeu { get => tempsDeJeu; set => tempsDeJeu = value; }
+        public float Pourcentage { get => pourcentage; set => pourcentage = value; }
+    }
+        
+}
---
+++ GamingCenter.PEYRON.BEAUDOUX/ClassLibrary/Entity/Jeu.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassLibrary.Entity
+{
+    public class Jeu
+    {
+        private int id;
+        private string nom;
+        private string description;
+        private DateTime dateSortie;
+
+        public Jeu(int id, string nom, string description, DateTime dateSortie)
+        {
+            this.Id = id;
+            this.Nom = nom;
+            this.Description = description;
+            this.DateSortie = dateSortie;
+        }
+
+        public int Id { get => id; set => id = value; }
+        public string Nom { get => nom; set => nom = value; }
+        public string Description { get => description; set => description = value; }
+        public DateTime DateSortie { get => dateSortie; set => dateSortie = value; }
+    }
+}
